e09585832d56c9c746b878822aed09be2852b45d
Justin <70568596+justinSmileDate@users.noreply.github.com>
2023-10-25 15:23:58 +0800

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,9 @@ def print_hi(name):
     print("adss")
     print("asdss")
 
-
+def print_hia(name):
+    print("asda")
+    
 # 按装订区域中的绿色按钮以运行脚本。
 if __name__ == '__main__':
     print_hi('PyCharm')
